1964b32709a5a48bacc5fa4c3e19efcc1cd72d56
parkdongmin <park@arcphili.co.jp>
2022-04-25 18:18:22 +0900

    ADD: Edit.js

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,24 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import {  DiaryStateContext} from "../App";
 
 const Edit = () => {
-
-    const [searchParams, setSearchParams] = useSearchParams();
-    const id = searchParams.get('id')
+    const navigate = useNavigate();
+    const { id} = useParams();
     console.log("id : ", id)
+    const diaryList = useContext(DiaryStateContext);
+    console.log(id);
+    console.log(diaryList);
+
+    useEffect(()=>{
+        if (diaryList.length >= 1) {
+            const targetDiary = diaryList.find(
+                (it) => parseInt(it.id) === parseInt(id) 
+            );
+            console.log(targetDiary)
+        }
+    },[id, diaryList]);
+
     return (
         <div>
             <h1>Edit</h1>
