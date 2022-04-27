dd6d7bf87e1d4b3b3dcfa244004da798d1c77216
parkdongmin <sayg1o1d@gmail.com>
2022-04-27 22:01:35 +0900

    ADD: MyHeader

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,1 +1,17 @@
+const MyHeader = ({headText, leftChild, rightChild}) => {
+    return (
+        <header>
+            <div className="head--btn--left">
+                {leftChild}
+            </div>
+            <div className="head--text">
+                {headText}
+            </div>
+            <div className="head--btn--right">
+                {rightChild}
+            </div>
+        </header>
+    )
+}
+
 export default MyHeader;
