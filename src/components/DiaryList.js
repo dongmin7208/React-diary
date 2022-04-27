3796e79d32ed3ca6139537e7a75d01c84265fe9d
parkdongmin <sayg1o1d@gmail.com>
2022-04-27 21:36:08 +0900

    ADD: DiaryList funtion

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,1 +1,80 @@
-export default DiaryList;
+import React,{useState} from "react"
+import {useNavigate} from "react-router-dom"
+import DiaryItem from "./DiaryItem"
+import MyButton from "./MyButton"
+
+const sortOptionList = [
+    {value: "latest", name:"new"},
+    {value : "oldest", name:"old"},
+]
+
+const filterOptionList = [
+    {value:"all", name:"all emotion"},
+    {value:"good", name:"good emotion"},
+    {value:"bad", name:"all bed"},
+]
+
+const ControlMenu = ({value, onChange, optionList}) => {
+    return(
+        <select className="ControlMenu" value={value} onChange={(e.target.value)}>
+            {optionList.map((it, index) => (
+                <option key={index} value={it.value}>
+                    {it.name}
+                </option>
+            ))}
+        </select>
+    )
+}
+
+const DiaryList = ({DiaryList}) => {
+    const navigate = useNavigate()
+
+    const [sortType, setSortType] = useState("latest")
+    const [filter, setFilter] = useState("all")
+
+    const filterCallback = (item) => {
+        if (filter === "good"){
+            return parseInt(item.emotion) >= 3
+        }
+        else{
+            return parseInt(item.emotion) < 3
+        }
+    }
+
+    const getProcessedDiaryList = () => {
+        const compare = (a,b) => {
+            if(sortType === "latest") {
+                return parseInt(b.date) - parseInt(a.date)
+            }
+            else{
+                return parseInt(a.date) - parseInt(b.date)
+            }
+        }
+
+        const copyList = JSON.parse(JSON.stringify(diaryList))
+        const filteredList = filter === 'all' ? copyList : copyList.filter((it) => filterCallback(it))
+        const sortedList = filteredList.sort(compare)
+        return sortedList
+    }
+
+    return (
+        <div className="DiaryList">
+            <div className="menu-wrapper">
+                <div className="left-col">
+                    <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
+                    <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}/>
+                </div>
+                <div className="right-col">
+                    <MyButton type={"positive"} text={"new Diary"} onClick={()=>navigate("/new")}/>
+                </div>
+            </div>
+            {getProcessedDiaryList().map((it) =>
+                <DiaryItem key={it.id} {...it} />
+            )}
+        </div>
+    )
+    DiaryList.defalutProps = {
+        diaryList : [],
+    }
+}
+export default DiaryList
