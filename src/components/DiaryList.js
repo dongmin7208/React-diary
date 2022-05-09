8b88b8d813e81fbc8fb7a76af51890ef30e7f455
parkdongmin <sayg1o1d@gmail.com>
2022-05-09 23:29:51 +0900

    divisionAlgorithmStep3

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,85 +1,101 @@
-import React,{ useState } from "react"
-import {useNavigate} from "react-router-dom"
-import DiaryItem from "./DiaryItem"
-import MyButton from "./MyButton"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
+import MyButton from "./MyButton";
 
 const sortOptionList = [
-    {value : "latest", name:"New"},
-    {value : "oldest", name:"Old"},
-]
+    { value: "latest", name: "New" },
+    { value: "oldest", name: "Old" },
+];
 
 const filterOptionList = [
-    {value:"all", name:"all emotion"},
-    {value:"good", name:"good emotion"},
-    {value:"bad", name:"all bed"},
-]
+    { value: "all", name: "all emotion" },
+    { value: "good", name: "good emotion" },
+    { value: "bad", name: "all bed" },
+];
 
-const ControlMenu = React.memo(({value, onChange, optionList}) => {
+const ControlMenu = React.memo(({ value, onChange, optionList }) => {
     return (
-        <select className="ControlMenu" value={value} onChange={(e)=>onChange(e.target.value)}>
+        <select
+            className="ControlMenu"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        >
             {optionList.map((it, index) => (
                 <option key={index} value={it.value}>
                     {it.name}
                 </option>
             ))}
         </select>
-    )
+    );
 });
 
-const DiaryList = ({diaryList}) => {
+const DiaryList = ({ diaryList }) => {
     const navigate = useNavigate();
-  
+
     const [sortType, setSortType] = useState("latest");
     const [filter, setFilter] = useState("all");
-  
+
     const filterCallback = (item) => {
-      if (filter === "good"){
-        return parseInt(item.emotion) >= 3;
-      }
-      else{
-        return parseInt(item.emotion) < 3;
-      }
-    }
-  
+        if (filter === "good") {
+            return parseInt(item.emotion) >= 3;
+        } else {
+            return parseInt(item.emotion) < 3;
+        }
+    };
+
     const getProcessedDiaryList = () => {
-      const compare = (a,b) => {
-        if(sortType === "latest"){
-          return parseInt(b.date) - parseInt(a.date);
-        }
-        else{
-          return parseInt(a.date) - parseInt(b.date);
-        }
-      };
-  
-      const copyList = JSON.parse(JSON.stringify(diaryList))
-  
-      const filteredList = filter === 'all' ? copyList : copyList.filter((it) => filterCallback(it));
-  
-      const sortedList = filteredList.sort(compare);
-      return sortedList;
-    }
-  
-  
+        const compare = (a, b) => {
+            if (sortType === "latest") {
+                return parseInt(b.date) - parseInt(a.date);
+            } else {
+                return parseInt(a.date) - parseInt(b.date);
+            }
+        };
+
+        const copyList = JSON.parse(JSON.stringify(diaryList));
+
+        const filteredList =
+            filter === "all"
+                ? copyList
+                : copyList.filter((it) => filterCallback(it));
+
+        const sortedList = filteredList.sort(compare);
+        return sortedList;
+    };
+
     return (
-      <div className="DiaryList">
-        <div className="menu-wrapper">
-          <div className="left-col">
-            <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
-            <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}/>
-          </div>
-          <div className="right-col">
-            <MyButton type={"positive"} text={"new diary"} onClick={()=>navigate("/new")} />
-          </div>
+        <div className="DiaryList">
+            <div className="menu-wrapper">
+                <div className="left-col">
+                    <ControlMenu
+                        value={sortType}
+                        onChange={setSortType}
+                        optionList={sortOptionList}
+                    />
+                    <ControlMenu
+                        value={filter}
+                        onChange={setFilter}
+                        optionList={filterOptionList}
+                    />
+                </div>
+                <div className="right-col">
+                    <MyButton
+                        type={"positive"}
+                        text={"new diary"}
+                        onClick={() => navigate("/new")}
+                    />
+                </div>
+            </div>
+            {getProcessedDiaryList().map((it) => (
+                <DiaryItem key={it.id} {...it} />
+            ))}
         </div>
-        {getProcessedDiaryList().map((it) =>
-          <DiaryItem key={it.id} {...it} />
-        )}
-      </div>
     );
-  };
-  
-  DiaryList.defaultProps = {
-    diaryList : [],
-  }
-  
-  export default DiaryList;
+};
+
+DiaryList.defaultProps = {
+    diaryList: [],
+};
+
+export default DiaryList;
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable react/jsx-no-comment-textnodes */
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DiaryStateContext } from "../App";
@@ -14,58 +12,72 @@ const Diary = () => {
 
     const [data, setData] = useState();
     const diaryList = useContext(DiaryStateContext);
-    console.log(id)
+    console.log(id);
 
-    useEffect(()=> {
-        if(diaryList.length >= 1){
+    useEffect(() => {
+        if (diaryList.length >= 1) {
             const targetDiary = diaryList.find(
                 (it) => parseInt(it.id) === parseInt(id)
             );
 
-            if(targetDiary) {
+            if (targetDiary) {
                 setData(targetDiary);
-            }else{
-                alert("存在してない日記です。")
-                navigate("/", {replace : true})
+            } else {
+                alert("存在してない日記です。");
+                navigate("/", { replace: true });
             }
         }
-    },[id, diaryList, navigate])
-    if(!data){
-        return <div className="DiaryPage">Loding...</div>
-    }else{
+    }, [id, diaryList, navigate]);
+    if (!data) {
+        return <div className="DiaryPage">Loding...</div>;
+    } else {
         const currentEmotionData = emotionList.find(
             (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
         );
         console.log(currentEmotionData);
-    
 
-    return (
-        <div className="DiaryPage">
-          <MyHeader 
-            headText={`${getStringDate(new Date(data.date))} 기록`} 
-            leftChild = {<MyButton text={"< back"} onClick={() => navigate(-1)}/>}
-          rightChild = {<MyButton text={"edit"} onClick={() => navigate(`/edit/${data.id}`)} />}
-          />
-          <article>
-            <section>
-              <h4>today emotion</h4>
-              <div className={["diary-img-wrapper", `diary-img-wrapper--${data.emotion}`].join(" ")}>
-                <img src={currentEmotionData.emotion_img} />
-                <div className="emotion-descript">
-                  {currentEmotionData.emotion_descript}
-                </div>
-              </div>
-            </section>
-            <section>
-              <h4>today diary</h4>
-              <div className="diary-content-wrapper">
-                <p>{data.content}</p>
-              </div>
-            </section>
-          </article>
-        </div>
-      )
+        return (
+            <div className="DiaryPage">
+                <MyHeader
+                    headText={`${getStringDate(new Date(data.date))} 기록`}
+                    leftChild={
+                        <MyButton
+                            text={"< back"}
+                            onClick={() => navigate(-1)}
+                        />
+                    }
+                    rightChild={
+                        <MyButton
+                            text={"edit"}
+                            onClick={() => navigate(`/edit/${data.id}`)}
+                        />
+                    }
+                />
+                <article>
+                    <section>
+                        <h4>today emotion</h4>
+                        <div
+                            className={[
+                                "diary-img-wrapper",
+                                `diary-img-wrapper--${data.emotion}`,
+                            ].join(" ")}
+                        >
+                            <img src={currentEmotionData.emotion_img} />
+                            <div className="emotion-descript">
+                                {currentEmotionData.emotion_descript}
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <h4>today diary</h4>
+                        <div className="diary-content-wrapper">
+                            <p>{data.content}</p>
+                        </div>
+                    </section>
+                </article>
+            </div>
+        );
     }
-  }
-  
-  export default Diary;
+};
+
+export default Diary;
